3f4e25c5a5d7196997ee8c8c0a7b175791e3c39a
Artem Balan <a.s.balan96@gmail.com>
2023-12-25 18:56:58 +0200

    Wire the beans using Autowired annotation

---
+++ src/main/java/org/example/ch2/ex2/Main.java
@@ -0,0 +1,17 @@
+package org.example.ch2.ex2;
+
+import org.example.ch2.ex2.beans.Person;
+import org.example.ch2.ex2.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person p = context.getBean(Person.class);
+
+        System.out.println("Person's name: " + p.getName());
+        System.out.println("Person's parrot: " + p.getParrot());
+    }
+}
