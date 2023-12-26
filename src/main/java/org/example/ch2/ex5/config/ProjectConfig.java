1beebff9cc3e9c25ace8266699ee5e050613c596
Artem Balan <a.s.balan96@gmail.com>
2023-12-26 19:10:53 +0200

    Another way of choosing bean using parameter name in bean

---
+++ src/main/java/org/example/ch2/ex5/config/ProjectConfig.java
@@ -0,0 +1,28 @@
+package org.example.ch2.ex5.config;
+
+
+import org.example.ch2.ex5.beans.Parrot;
+import org.example.ch2.ex5.beans.Person;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.example.ch2.ex5.beans")
+public class ProjectConfig {
+
+    @Bean
+    public Parrot parrot1() {
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    public Parrot parrot2() {
+        Parrot p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+}
