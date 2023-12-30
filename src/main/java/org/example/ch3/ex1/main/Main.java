32232bf809b72bdad839a66e0c92e24e7dccd6d6
Artem Balan <a.s.balan96@gmail.com>
2023-12-30 21:36:35 +0200

    change method main to check app work

--- src/main/java/org/example/ch3/ex1/main/Main.java
+++ src/main/java/org/example/ch3/ex1/main/Main.java
@@ -1,22 +1,19 @@
 package org.example.ch3.ex1.main;
 
+import org.example.ch3.ex1.configuration.ProjectConfiguration;
 import org.example.ch3.ex1.model.Comment;
-import org.example.ch3.ex1.proxies.EmailCommentNotificationProxy;
-import org.example.ch3.ex1.repositories.DBCommentRepository;
 import org.example.ch3.ex1.services.CommentService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        var commentRepository = new DBCommentRepository();
-        var commentNotificationProxy = new EmailCommentNotificationProxy();
-
-        var commentService = new CommentService(commentRepository,
-                commentNotificationProxy);
-
+        var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
         var comment = new Comment();
         comment.setAuthor("Laurentiu");
         comment.setText("Demo comment");
 
+        var commentService = context.getBean(CommentService.class);
         commentService.publishComment(comment);
     }
 }
+
