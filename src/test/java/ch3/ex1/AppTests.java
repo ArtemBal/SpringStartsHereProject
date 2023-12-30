cacdcc86789a088c426796c5aa6643188dd23c9f
Artem Balan <a.s.balan96@gmail.com>
2023-12-30 21:36:46 +0200

    write tests

--- src/test/java/ch3/ex1/AppTests.java
+++ src/test/java/ch3/ex1/AppTests.java
@@ -6,25 +6,33 @@ import org.example.ch3.ex1.repositories.CommentRepository;
 import org.example.ch3.ex1.services.CommentService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-public class AppTests {         // TODO: Mock tests doesn't work with 17 version of java but works fine with 11
+@ExtendWith(MockitoExtension.class)
+public class AppTests {
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @Mock
+    private CommentNotificationProxy commentNotificationProxy;
+
+    @InjectMocks
+    private CommentService commentService;
 
     @Test
-    @DisplayName("Verify that CommentService correctly delegates the " +
-            "responsibilities to the repository and proxy objects.")
+    @DisplayName("Verify that dependencies of the CommentService object are correctly called.")
     public void testCommentService() {
-        var comment = mock(Comment.class);
-        var commentRepository = mock(CommentRepository.class);
-        var commentNotificationService = mock(CommentNotificationProxy.class);
-
-        var commentService = new CommentService(commentRepository, commentNotificationService);
+        var comment = new Comment();
 
         commentService.publishComment(comment);
 
         verify(commentRepository).storeComment(comment);
-        verify(commentNotificationService).sendComment(comment);
+        verify(commentNotificationProxy).sendComment(comment);
     }
 }
