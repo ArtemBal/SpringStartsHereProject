c2168adfdc5504e19e1b0fd56313312194936b5e
Artem Balan <a.s.balan96@gmail.com>
2024-01-02 21:09:35 +0200

    write tests

---
+++ src/test/java/ch3/ex2/AppTests.java
@@ -0,0 +1,40 @@
+package ch3.ex2;
+
+import org.example.ch3.ex2.model.Comment;
+import org.example.ch3.ex2.proxies.CommentNotificationProxy;
+import org.example.ch3.ex2.repositories.CommentRepository;
+import org.example.ch3.ex2.services.CommentService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class AppTests {
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @Mock
+    private CommentNotificationProxy commentNotificationProxy;
+
+    @InjectMocks
+    private CommentService commentService;
+
+    @Test
+    @DisplayName("Verify that dependencies of the " +
+            "CommentService object are correctly called.")
+    public void testCommentService() {
+        var comment = new Comment();
+
+        commentService.publishComment(comment);
+
+        verify(commentRepository).storeComment(comment);
+        verify(commentNotificationProxy).sendComment(comment);
+    }
+
+}
