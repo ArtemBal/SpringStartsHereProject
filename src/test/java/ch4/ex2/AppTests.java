7ae17eda6d02d934419cf56bd83041eee944fb25
Artem Balan <a.s.balan96@gmail.com>
2024-01-11 21:45:51 +0200

    write test

---
+++ src/test/java/ch4/ex2/AppTests.java
@@ -0,0 +1,44 @@
+package ch4.ex2;
+
+import org.example.ch4.ex2.condig.ProjectConfig;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { ProjectConfig.class })
+public class AppTests {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    @DisplayName("Verify that CommentService created")
+    public void testCommentServiceIsSingleton() {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        assertEquals("CommentService instance created!\n", outContent.toString());
+    }
+
+}
