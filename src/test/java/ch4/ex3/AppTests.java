6ef367ca97d3deb2e6c87d03e3a88e02d6f75a9d
Artem Balan <a.s.balan96@gmail.com>
2024-01-12 20:19:11 +0200

    write tests

---
+++ src/test/java/ch4/ex3/AppTests.java
@@ -0,0 +1,32 @@
+package ch4.ex3;
+
+import org.example.ch4.ex3.condig.ProjectConfig;
+import org.example.ch4.ex3.services.CommentService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { ProjectConfig.class })
+public class AppTests {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    @DisplayName("Verify that CommentService every time you request an instance" +
+            " from the Spring context, you get a new instance")
+    public void testCommentServiceIsPrototype() {
+        var cs1 = context.getBean("commentService", CommentService.class);
+        var cs2 = context.getBean("commentService", CommentService.class);
+
+        assertNotEquals(cs1, cs2);
+    }
+
+}
