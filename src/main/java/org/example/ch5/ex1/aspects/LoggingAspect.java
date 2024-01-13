00ce6be751897a15282f7968a0235fca595acfe1
Artem Balan <a.s.balan96@gmail.com>
2024-01-13 23:19:32 +0200

    use stereotype annotations instead of @Bean

--- src/main/java/org/example/ch5/ex1/aspects/LoggingAspect.java
+++ src/main/java/org/example/ch5/ex1/aspects/LoggingAspect.java
@@ -3,10 +3,12 @@ package org.example.ch5.ex1.aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
 
 @Aspect
+@Component
 public class LoggingAspect {
 
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
