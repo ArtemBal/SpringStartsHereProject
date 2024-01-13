93e415b908d275029b63088166188a11cc4508f2
Artem Balan <a.s.balan96@gmail.com>
2024-01-13 23:10:10 +0200

    add aspect

--- src/main/java/org/example/ch5/ex1/config/ProjectConfig.java
+++ src/main/java/org/example/ch5/ex1/config/ProjectConfig.java
@@ -1,9 +1,18 @@
 package org.example.ch5.ex1.config;
 
+import org.example.ch5.ex1.aspects.LoggingAspect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan("org.example.ch5.ex1.services")
+@EnableAspectJAutoProxy
 public class ProjectConfig {
+
+    @Bean
+    public LoggingAspect aspect() {
+        return new LoggingAspect();
+    }
 }
